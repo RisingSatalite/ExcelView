e88a2bbc21e1ec48d1a5985d28edac289d53ad57
RisingSatalite <saihajmann07@gmail.com>
2025-07-23 20:07:06 -0600

    simple project

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ExcelView;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿namespace ExcelView;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object? sender, EventArgs e)
+	{
+		count++;
+
+		if (count == 1)
+			CounterBtn.Text = $"Clicked {count} time";
+		else
+			CounterBtn.Text = $"Clicked {count} times";
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+
+	public async void SelectFile()
+	{
+		try
+		{
+			var result = await FilePicker.Default.PickAsync();
+			if (result != null)
+			{
+				// File was picked
+				string fileName = result.FileName;
+				// You can access the file stream with result.OpenReadAsync()
+			}
+		}
+		catch (Exception ex)
+		{
+			Console.WriteLine(ex);
+			// Handle exceptions (e.g., user canceled or permissions issue)
+		}
+    }
+}
